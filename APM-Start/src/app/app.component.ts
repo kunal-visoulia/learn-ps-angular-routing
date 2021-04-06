ac409b136286b533352c10ea33d6190cb625174c
kunal-visoulia <kunalvisoulia@gmail.com>
2021-04-06 23:33:08 +0530

    added spinner and understood route navigation lc

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { slideInAnimation } from './app.animation';
 import { AuthService } from './user/auth.service';
-import {Router} from '@angular/router';
+import {Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel} from '@angular/router';
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
@@ -10,6 +10,7 @@ import {Router} from '@angular/router';
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
+  loading = true;
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -22,8 +23,23 @@ export class AppComponent {
     return '';
   }
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {
+    router.events.subscribe((routerEvent: Event)=>{
+      this.checkRouterEvent(routerEvent);
+    });
+   }
+   
+   checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
 
+    if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel ||
+        routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
   logOut(): void {
     this.authService.logout();
     console.log('Log out');
